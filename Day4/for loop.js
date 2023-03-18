3d4973f45ddd8141879d3c05620465e6afc03e88
ganesamoorthy-g <gurunathganesh6@gmail.com>
2023-03-18 14:18:24 +0530

    first commit

---
+++ Day4/for loop.js
@@ -0,0 +1,25 @@
+// let book={
+// purchaserName :'ganesh',
+// price:65.34
+
+// }
+// console.log(Object.entries(book));
+
+//for ...of loop
+
+// let arrar=[1,2,3,4,5];
+// for(let value of arrar){
+//     console.log(value, ':' , arrar);
+// }
+
+// let char='ganesamoorthy';
+// for(let index of char){
+//     console.log(index , ':' ,char);
+// }
+
+//for...in looping
+
+// let numbers='ganesamoorthy';
+// for(let char in numbers){
+//     console.log(char,':',numbers[char])
+// };
